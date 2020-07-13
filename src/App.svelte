296b99b5d48ad03cc151a60270c8d83d4873442e
Christian Consuelo <christian.consuelo2@gmail.com>
2020-07-13 16:44:32 -0500

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+// Components
+import ControlBar from './components/ControlBar.svelte';
+
+// Containers
+import PatternPicker from './containers/PatternPicker.svelte';
+import Universe from './containers/Universe.svelte';
+</script>
+
+<style>
+.AppContainer {
+	display: grid;
+	grid-template-columns: 2fr 1fr;
+}
+</style>
+
+<div class="App">
+	<ControlBar />
+
+	<div class="AppContainer">
+		<div class="AppContainer-Universe">
+			<Universe />
+		</div>
+		<div class="AppContainer-PatternPicker">
+			<PatternPicker />
+		</div>
+	</div>
+</div>
---
+++ src/components/ControlBar.svelte
@@ -0,0 +1,3 @@
+<div class="ControlBar">
+  ControlBar
+</div>
