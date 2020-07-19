5b0862a63471da217f3475a09afbcb57a6130408
Christian Consuelo <christian.consuelo2@gmail.com>
2020-07-19 16:50:03 -0500

    Fix original initial universe size

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store';
 
 export const universe = writable(null);
-export const universeSize = writable(4);
+export const universeSize = writable(25);
 export const active = writable(false);
 export const interval = writable(null);
 export const speed = writable(100);
